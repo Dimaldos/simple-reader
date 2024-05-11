2eebd5157590c2fc688e3ae3811565c4860ead0d
Dimaldos <dima@hakobyan.ru>
2024-05-11 22:09:17 +0300

    init

---
+++ books-service-db/src/main/java/ru/dimaldos/simplereader/booksservicedb/BooksServiceDbApplication.java
@@ -0,0 +1,13 @@
+package ru.dimaldos.simplereader.booksservicedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksServiceDbApplication.class, args);
+    }
+
+}
---
+++ books-service-db/src/test/java/ru/dimaldos/simplereader/booksservicedb/BooksServiceDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dimaldos.simplereader.booksservicedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
