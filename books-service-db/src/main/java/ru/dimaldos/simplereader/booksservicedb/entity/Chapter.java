d1b4a968c7de8292fd3b3a9245263fbca55d0103
Dimaldos <dima@hakobyan.ru>
2024-05-18 18:12:47 +0300

    add content to chapters

--- books-service-db/src/main/java/ru/dimaldos/simplereader/booksservicedb/entity/Chapter.java
+++ books-service-db/src/main/java/ru/dimaldos/simplereader/booksservicedb/entity/Chapter.java
@@ -1,12 +1,16 @@
 package ru.dimaldos.simplereader.booksservicedb.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,6 +18,8 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Data
 @Entity
@@ -22,6 +28,11 @@ import lombok.NoArgsConstructor;
 @Table(
         name = "chapters",
         schema = "bookshelf"
+)
+@SecondaryTable(
+        name = "contents",
+        schema = "bookshelf",
+        pkJoinColumns = @PrimaryKeyJoinColumn(name = "id")
 )
 public class Chapter {
 
@@ -43,5 +54,9 @@ public class Chapter {
             nullable = false
     )
     private Volume volume;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(table = "contents", name = "content")
+    private JsonNode content;
 
 }
